a4c720047b9b3135118323f0234b67c0ec307fcb
phaux <phaux4@gmail.com>
2019-08-17 18:26:40 +0200

    chore: Big rewrite

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,28 @@
-import { spawn } from "child_process"
+import { ChildProcess, spawn, StdioOptions } from "child_process"
 import { createReadStream, createWriteStream, unlink } from "fs"
 import { tmpdir } from "os"
 import { join } from "path"
-import { PassThrough } from "stream"
+import { PassThrough, Readable, Writable } from "stream"
 import { promisify } from "util"
+import { debug } from "debug"
 
-const debug = require("debug")("ffmpeg-stream")
+const dbg = debug("ffmpeg-stream")
+const { FFMPEG_PATH = "ffmpeg" } = process.env
+const EXIT_CODES = [0, 255]
 
-async function getTmpPath(prefix = "", suffix = "") {
+function debugStream(stream: Readable | Writable, name: string) {
+  stream.on("error", err => {
+    dbg(`${name} error: ${err}`)
+  })
+  stream.on("data", data => {
+    dbg(`${name} data: ${data.length} bytes`)
+  })
+  stream.on("finish", () => {
+    dbg(`${name} finish`)
+  })
+}
+
+function getTmpPath(prefix = "", suffix = "") {
   const dir = tmpdir()
   const id = Math.random()
     .toString(32)
@@ -15,290 +30,285 @@ async function getTmpPath(prefix = "", suffix = "") {
   return join(dir, `${prefix}${id}${suffix}`)
 }
 
-const { FFMPEG_PATH = "ffmpeg" } = process.env
+type Options = Record<string, string | number | boolean | null | undefined>
 
-type InputOpts = Record<string, string>
-
-type OutputOpts = Record<string, string>
-
-interface ExtraOpts {
-  /**
-   * When set to true, a temporary file will be used transparently,
-   * instead of piping to/from ffmpeg process directly.
-   * Useful when ffmpeg fails with particular format when streaming.
-   */
-  useTemporaryFile?: boolean
+function getArgs(options: Options): string[] {
+  const args: string[] = []
+  for (const option in options) {
+    const value = options[option]
+    if (value != null && value !== false) {
+      args.push(`-${option}`)
+      if (typeof value != "boolean") {
+        args.push(String(value))
+      }
+    }
+  }
+  return args
 }
 
-// prettier-ignore
-type IO =
-  & (
-    | { type: "in"; opts: InputOpts }
-    | { type: "out"; opts: OutputOpts }
-  )
-  & (
-    | { mode: "file"; file: string }
-    | { mode: "buffer" | "stdio"; stream: PassThrough }
-  );
+interface Pipe {
+  readonly type: "input" | "output"
+  readonly options: Options
+  readonly file: string
+  onBegin?(): Promise<void>
+  onSpawn?(process: ChildProcess): void
+  onFinish?(): Promise<void>
+}
+
+/** @deprecated Construct [[Converter]] class directly */
+export function ffmpeg() {
+  return new Converter()
+}
 
 export class Converter {
-  private io: IO[] = []
-  private proc: any = null
-  private started = false
-  private killed = false
-  private cmd: any = []
-  private stdio: any
-  private last = ""
-  private log: any = []
-  private section = 0
+  private fdCount = 0
+  private pipes: Pipe[] = []
+  private process?: ChildProcess
 
-  createInputStream(inputOpts: InputOpts, miscOpts: ExtraOpts = {}) {
-    const stream = new PassThrough()
-    const mode = miscOpts.useTemporaryFile ? "buffer" : "stdio"
-    this.io.push({ type: "in", mode, opts: inputOpts, stream })
-    return stream
-  }
-
-  createInputFromFile(file: string, opts: InputOpts) {
-    this.io.push({ type: "in", mode: "file", file, opts })
-  }
-
-  createOutputStream(outputOpts: OutputOpts, miscOpts: ExtraOpts = {}) {
-    const stream = new PassThrough()
-    const mode = miscOpts.useTemporaryFile ? "buffer" : "stdio"
-    this.io.push({ type: "out", mode, opts: outputOpts, stream })
-    return stream
-  }
-
-  createOutputToFile(file: string, opts: OutputOpts) {
-    this.io.push({ type: "out", mode: "file", file, opts })
+  private getUniqueFd() {
+    return this.fdCount++ + 3
   }
 
   /** @deprecated Use [[createInputStream]] or [[createInputFromFile]] */
   input(arg0: any, arg1: any) {
     const [file, opts] =
       typeof arg0 == "string" ? [arg0, arg1] : [undefined, arg0]
-    if (file) return this.createInputFromFile(file, opts)
-    const buffer = opts.buffer
-    delete opts.buffer
-    return this.createInputStream(opts, buffer)
+
+    if (file != null) {
+      return this.createInputFromFile(file, opts)
+    }
+    if (opts.buffer) {
+      delete opts.buffer
+      return this.createBufferedInputStream(opts)
+    }
+    return this.createInputStream(opts)
   }
 
   /** @deprecated Use [[createOutputStream]] or [[createOutputToFile]] */
   output(arg0: any, arg1: any) {
     const [file, opts] =
       typeof arg0 == "string" ? [arg0, arg1] : [undefined, arg0]
-    if (file) return this.createOutputToFile(file, opts)
-    const buffer = opts.buffer
-    delete opts.buffer
-    return this.createOutputStream(opts, buffer)
+
+    if (file != null) {
+      return this.createOutputToFile(file, opts)
+    }
+    if (opts.buffer) {
+      delete opts.buffer
+      return this.createBufferedOutputStream(opts)
+    }
+    return this.createOutputStream(opts)
+  }
+
+  createInputFromFile(file: string, options: Options): void {
+    this.pipes.push({
+      type: "input",
+      options,
+      file
+    })
+  }
+
+  createOutputToFile(file: string, options: Options): void {
+    this.pipes.push({
+      type: "output",
+      options,
+      file
+    })
+  }
+
+  createInputStream(options: Options): Writable {
+    const stream = new PassThrough()
+    const fd = this.getUniqueFd()
+    this.pipes.push({
+      type: "input",
+      options,
+      file: `pipe:${fd}`,
+      onSpawn: process => {
+        const stdio = process.stdio[fd]
+        if (stdio == null) throw Error(`input ${fd} is null`)
+        debugStream(stream, `input ${fd}`)
+        if (!("write" in stdio)) throw Error(`input ${fd} is not writable`)
+        stream.pipe(stdio)
+      }
+    })
+
+    return stream
+  }
+
+  createOutputStream(options: Options): Readable {
+    const stream = new PassThrough()
+    const fd = this.getUniqueFd()
+    this.pipes.push({
+      type: "output",
+      options,
+      file: `pipe:${fd}`,
+      onSpawn: process => {
+        const stdio = process.stdio[fd]
+        if (stdio == null) throw Error(`output ${fd} is null`)
+        debugStream(stdio, `output ${fd}`)
+        stdio.pipe(stream)
+      }
+    })
+    return stream
+  }
+
+  createBufferedInputStream(options: Options): Writable {
+    const stream = new PassThrough()
+    const file = getTmpPath("ffmpeg-")
+    this.pipes.push({
+      type: "input",
+      options,
+      file,
+      onBegin: async () => {
+        await new Promise((resolve, reject) => {
+          const writer = createWriteStream(file)
+          stream.pipe(writer)
+          stream.on("end", () => {
+            dbg("input buffered stream end")
+            resolve()
+          })
+          stream.on("error", err => {
+            dbg(`input buffered stream error: ${err}`)
+            return reject(err)
+          })
+        })
+      },
+      onFinish: async () => {
+        await promisify(unlink)(file)
+      }
+    })
+    return stream
+  }
+
+  createBufferedOutputStream(options: Options): Writable {
+    const stream = new PassThrough()
+    const file = getTmpPath("ffmpeg-")
+    this.pipes.push({
+      type: "output",
+      options,
+      file,
+      onFinish: async () => {
+        await new Promise((resolve, reject) => {
+          const reader = createReadStream(file)
+          reader.pipe(stream)
+          reader.on("end", () => {
+            dbg("output buffered stream end")
+            resolve()
+          })
+          return reader.on("error", err => {
+            dbg(`output buffered stream error: ${err}`)
+            reject(err)
+          })
+        })
+        await promisify(unlink)(file)
+      }
+    })
+    return stream
+  }
+
+  private getStdioArg(): StdioOptions {
+    return [
+      "ignore",
+      "ignore",
+      "pipe",
+      ...Array<"pipe">(this.fdCount).fill("pipe")
+    ]
+  }
+
+  private getSpawnArgs() {
+    const stdio = this.getStdioArg()
+    const command: string[] = []
+
+    for (const pipe of this.pipes) {
+      if (pipe.type != "input") continue
+      command.push(...getArgs(pipe.options))
+      command.push("-i", pipe.file)
+    }
+    for (const pipe of this.pipes) {
+      if (pipe.type != "output") continue
+      command.push(...getArgs(pipe.options))
+      command.push(pipe.file)
+    }
+
+    return { command, stdio }
+  }
+
+  async run(): Promise<void> {
+    const pipes: Pipe[] = []
+    try {
+      for (const pipe of this.pipes) {
+        dbg(`prepare ${pipe.type}`)
+        if (pipe.onBegin != null) await pipe.onBegin()
+        pipes.push(pipe)
+      }
+
+      const { command, stdio } = this.getSpawnArgs()
+      dbg(`spawn: ${FFMPEG_PATH} ${command.join(" ")}`)
+      this.process = spawn(FFMPEG_PATH, command, { stdio })
+      const finished = this.handleProcess()
+
+      for (const pipe of this.pipes) {
+        if (pipe.onSpawn != null) pipe.onSpawn(this.process)
+      }
+
+      await finished
+    } finally {
+      for (const pipe of pipes) {
+        if (pipe.onFinish != null) await pipe.onFinish()
+      }
+    }
+  }
+
+  private async handleProcess() {
+    await new Promise<void>((resolve, reject) => {
+      let lastLogLine = ""
+      let logSectionNum = 0
+      const logLines: string[] = []
+
+      if (this.process == null) return reject(Error(`Converter not started`))
+
+      if (this.process.stderr != null) {
+        this.process.stderr.setEncoding("utf8")
+
+        this.process.stderr.on("data", data => {
+          // include last line from previous event
+          const buffer = (lastLogLine += data)
+          const lines = buffer.split(/\r\n|\r|\n/)
+          // save last line because it might be unfinished
+          lastLogLine = lines.pop()!
+
+          for (const line of lines) {
+            // skip empty lines
+            if (line.match(/^\s*$/)) continue
+            // if not indented: increment section counter
+            if (!line.match(/^ /)) logSectionNum++
+            // only log sections following the first one
+            if (logSectionNum >= 2) {
+              dbg(`log: ${line}`)
+              logLines.push(line)
+            }
+          }
+        })
+      }
+
+      this.process.on("error", err => {
+        dbg(`error: ${err}`)
+        return reject(err)
+      })
+
+      this.process.on("exit", (code, signal) => {
+        dbg(`exit: code=${code} sig=${signal}`)
+        if (code == null) return resolve()
+        if (EXIT_CODES.includes(code)) return resolve()
+        const log = [...logLines, lastLogLine]
+          .map(line => `  ${line}`)
+          .join("\n")
+        reject(Error(`Converting failed\n${log}`))
+      })
+    })
   }
 
   kill() {
-    this.killed = true
-    if (this.proc) {
-      return this.proc.kill("SIGINT")
-    }
+    if (this.process == null) throw Error(`Converter not started`)
+    this.process.kill()
   }
-
-  run() {
-    if (this.started) {
-      return Promise.reject(new Error("Already started"))
-    }
-    this.started = true
-    this.cmd = []
-    this.stdio = ["ignore", "ignore", "pipe"]
-
-    return Promise.resolve()
-      .then(() => {
-        const p = this.io
-          .filter(io => io.mode === "buffer")
-          .map(io => getTmpPath("ffmpeg-").then(name => (io.tmpfile = name)))
-        return Promise.all(p)
-      })
-      .then(() => {
-        // build stdio arg
-        let fd = 3
-        return this.io
-          .filter(io => io.mode === "stdio")
-          .forEach(io => {
-            io.fd = fd++
-            return this.stdio.push("pipe")
-          })
-      })
-      .then(() => {
-        // build command string
-        const mkcmd = type => io => {
-          for (let o in io.opts) {
-            const v = io.opts[o]
-            if (!(v == null) && v !== false && v !== "") {
-              this.cmd.push(`-${o}`)
-              if (v !== true) {
-                this.cmd.push(v)
-              }
-            }
-          }
-          if (type === "in") {
-            this.cmd.push("-i")
-          }
-          return this.cmd.push(
-            (() => {
-              switch (io.mode) {
-                case "file":
-                  return io.file
-                case "buffer":
-                  return io.tmpfile
-                case "stdio":
-                  return `pipe:${io.fd}`
-              }
-            })()
-          )
-        }
-        this.io.filter(io => io.type === "in").forEach(mkcmd("in"))
-        return this.io.filter(io => io.type === "out").forEach(mkcmd("out"))
-      })
-      .then(() => {
-        // consume all input buffered streams
-        const p = this.io
-          .filter(io => io.type === "in" && io.mode === "buffer")
-          .map(
-            io =>
-              new Promise(function(ok, fail) {
-                io.stream.pipe(createWriteStream(io.tmpfile))
-                io.stream.on("end", function() {
-                  debug("input buffered stream end")
-                  return ok()
-                })
-                return io.stream.on("error", function(err) {
-                  debug(`input buffered stream error: ${err}`)
-                  return fail(err)
-                })
-              })
-          )
-        return Promise.all(p)
-      })
-      .then(() => {
-        return new Promise((ok, fail) => {
-          // buffer last line of output in case it was incomplete
-          this.last = ""
-          // buffer output lines for error reporting purposes
-          this.log = []
-          // this remembers the 'section' of ffmpeg output so we can skip
-          // the parts where it just prints versions of codecs
-          this.section = 0
-
-          debug(`spawn: ${FFMPEG_PATH} ${this.cmd.join(" ")}`)
-
-          this.proc = spawn(FFMPEG_PATH, this.cmd, { stdio: this.stdio })
-
-          if (this.killed) {
-            setTimeout(() => this.proc.kill("SIGINT"))
-          }
-
-          // processing ffmpeg output
-
-          this.proc.stderr.setEncoding("utf8")
-
-          this.proc.stderr.on("data", data => {
-            const buf = (this.last += data)
-            const lines = buf.split(/\r\n|\n|\r/)
-            this.last = lines.pop() // save last line to buffer
-            return lines.forEach(line => {
-              // skip empty lines
-              if (line.match(/^\s*$/)) {
-                return
-              }
-              // if not indented: increment section counter
-              if (!line.match(/^ /)) {
-                this.section++
-              }
-              // only log sections following the first one
-              if (this.section >= 2) {
-                debug(`log: ${line}`)
-                return this.log.push(line)
-              }
-            })
-          })
-
-          this.proc.on("error", function(err) {
-            debug(`error: ${err}`)
-            return fail(err)
-          })
-
-          this.proc.on("exit", (code, sig) => {
-            debug(`exit: code=${code} sig=${sig}`)
-            if (!code || code === 255) {
-              return ok()
-            } else {
-              this.log.push(this.last) // push the last line of logs
-              return fail(
-                new Error("Transcoding failed:\n  " + this.log.join("\n  "))
-              )
-            }
-          })
-
-          return this.io
-            .filter(io => io.mode === "stdio")
-            .forEach(io => {
-              this.proc.stdio[io.fd].on("error", err =>
-                debug(`${io.type}put stream ${io.fd} error: ${err}`)
-              )
-              //io.stream.emit 'error', err
-
-              //if io.type is 'out'
-              this.proc.stdio[io.fd].on("data", data =>
-                debug(
-                  `${io.type}put stream ${io.fd} data: ${data.length} bytes`
-                )
-              )
-
-              this.proc.stdio[io.fd].on("finish", () =>
-                debug(`${io.type}put stream ${io.fd} finish`)
-              )
-
-              switch (io.type) {
-                case "in":
-                  return io.stream.pipe(this.proc.stdio[io.fd])
-                case "out":
-                  return this.proc.stdio[io.fd].pipe(io.stream)
-              }
-            })
-        })
-      })
-      .then(() => {
-        // read all output buffered streams
-        const p = this.io
-          .filter(io => io.type === "out" && io.mode === "buffer")
-          .map(
-            io =>
-              new Promise(function(ok, fail) {
-                const f = createReadStream(io.tmpfile)
-                f.pipe(io.stream)
-                f.on("end", function() {
-                  debug("output buffered stream end")
-                  return ok()
-                })
-                return f.on("error", function(err) {
-                  debug(`output buffered stream error: ${err}`)
-                  return fail(err)
-                })
-              })
-          )
-        return Promise.all(p)
-      })
-      .then(() => {
-        // remove buffer files
-
-        let p
-        return (p = this.io
-          .filter(io => io.mode === "buffer")
-          .map(io => promisify(unlink)(io.tmpfile)))
-      })
-      .then(function() {})
-  }
-}
-
-/** @deprecated Construct [[Converter]] class directly */
-export function ffmpeg() {
-  return new Converter()
 }
